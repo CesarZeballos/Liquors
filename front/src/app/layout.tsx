b0bcad67c1afda499663d0bfa83deee33b42249a
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-06-12 10:31:38 +0200

    layout y otras pages iniciales

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NavBar } from "@/components/navBar/navbar";
+import Providers from "@/store/providers";
+import { Footer } from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <NavBar />
+          <hr></hr>
+          {/* fuente plus-jakarta */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+           {/* ----------- */}
+          {children}
+          <Footer/>
+        </Providers>
+      </body>
     </html>
   );
 }
